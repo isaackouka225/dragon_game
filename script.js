ccbb059cba643df129b4007b4148bb310560f6ad
isaackouka225 <isaacsavadogo225@mail.com>
2023-10-18 12:24:39 +0200

    final modification

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-game = document.getElementById(game)
-punchButton = document.getElementById(punchButton)
-kick = document.getElementById(kickButton)
-specialButton = document.getElementById(specialButton)
-playerNameEl = document.getElementById(playerName)
-playerHealthEl = document.getElementById(playerHealth)
-playerHealthBar = document.getElementById(playerHealthBar)
-dragonNameEl = document.getElementById (dragonName)
-dragonHealthEl = document.getElementById(dragonHealth)
-dragonHealthBar = document.getElementById(dragonHealthBar)
+let game = document.getElementById("game")
+let punchButton = document.getElementById("punchButton")
+let kick = document.getElementById("kickButton")
+let specialButton = document.getElementById("specialButton")
+let playerNameEl = document.getElementById("playerName")
+let playerHealth = document.getElementById("playerHealth")
+let playerHealthBar = document.getElementById("playerHealthBar")
+let dragonName = document.getElementById ("dragonName")
+let dragonHealth = document.getElementById("dragonHealth")
+let dragonHealthBar = document.getElementById("dragonHealthBar")
 
 
-special = 7
-playerTour = true
+let special = 7
+let playerTour = true
 
 
 let player = {
@@ -30,35 +30,35 @@ let dragon = {
 
 
 function showCharacterState() {
-    playerNameElement.textContent = player.name;
-    playerHealthElement.textContent = `Santé : ${player.health}`;
+    playerName.textContent = player.name;
+    playerHealth.textContent = `Santé : ${player.health}`;
     playerHealthBar.style.width = `${(player.health / 200) * 100}%`;
-    dragonNameElement.textContent = dragon.name;
-    dragonHealthElement.textContent = `Santé : ${dragon.health}`;
+    dragonName.textContent = dragon.name;
+    dragonHealth.textContent = `Santé : ${dragon.health}`;
     dragonHealthBar.style.width = `${(dragon.health / 250) * 100}%`;
 }
 
 function showGameText(message) {
-    gameText.textContent = message;
+    game.textContent = message;
 }
 
 function playerAttack(attack) {
     showGameText("selectionner votre attaque")
-    if (attack == "punch") {
+    if (attack === "punch") {
         showGameText(`vous avez donné une punch à ${dragon.name}`)
         dragon.health = dragon.health - 20
     }
-    else if (attack == "kick") {
+    else if (attack === "kick") {
         showGameText(`vous avez donné un kick à ${dragon.name}`)
         dragon.health = dragon.health - 25
     }
 
-    else if (attack == "special") {
+    else if (attack === "special") {
         showGameText(`vous avez fait votre combo special sur ${dragon.name}`)
         dragon.health = dragon.health - 35
         special = special - 1
     }
-    else if (attack == "null") {
+    else if (attack === "null") {
         showGameText("vous recevez des dommages")
         player.health = player.health - 10
     }
@@ -66,15 +66,15 @@ function playerAttack(attack) {
 }
 
 function specialAttack() {
-    if (special == 0) {
+    if (special === 0) {
         showGameText("vous n'avez plus d'attaques spéciales")
     }
 }
 
 function dragonAttack() {
-    dragonDammage = 20
-    dragonSpecialDammage = 30
-    dragonAttackDammage = Math.random()
+    let dragonDammage = 20
+    let dragonSpecialDammage = 30
+    let dragonAttackDammage = Math.random()
     if (dragonAttackDammage < 0.7) {
         player.health = player.health - dragonDammage
         showGameText(`vous recevez une attaque de ${dragon.name}`)
@@ -83,11 +83,11 @@ function dragonAttack() {
         player.health = player.health - dragonSpecialDammage
         showGameText(`vous recevez la spéciale de ${dragon.name}`)
     }
-    showCharacterState
+    showCharacterState()
 }
 
 
-punchButton.addEventListener("click", function () {
+punchButton.addEventListener ("click", function () {
     if (playerTour) {
         playerAttack("punch");
         playerTour = false;
@@ -95,7 +95,7 @@ punchButton.addEventListener("click", function () {
     }
 });
 
-kickButton.addEventListener("click", function () {
+kickButton.addEventListener ("click", function () {
     if (playerTour) {
         playerAttack("kick");
         playerTour = false;
@@ -103,7 +103,7 @@ kickButton.addEventListener("click", function () {
     }
 });
 
-specialButton.addEventListener("click", function () {
+specialButton.addEventListener ("click", function () {
     if (playerTour) {
         playerAttack("special");
         playerTour = false;
@@ -121,9 +121,8 @@ function playRound() {
 
 
 function gameLoop() {
-        showCharacterState
-        playRound()
+        showCharacterState();
+        playRound();
     }
 
-    
-gameLoop()
+    gameLoop ();
