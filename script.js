82c09c81377eb130a7e1653124e09a6c09b1676f
isaackouka225 <isaacsavadogo225@mail.com>
2023-10-18 16:17:26 +0200

    new terminal modication

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-game = document.getElementById("game")
-punchButton = document.getElementById("punchButton")
-kickButton = document.getElementById("kickButton")
-specialButton = document.getElementById("specialButton")
-playerNameEl = document.getElementById("playerName")
-playerHealthEl = document.getElementById("playerHealth")
-playerHealthBar = document.getElementById("playerHealthBar")
-dragonNameEl = document.getElementById ("dragonName")
-dragonHealthEl = document.getElementById("dragonHealth")
-dragonHealthBar = document.getElementById("dragonHealthBar")
+let game = document.getElementById("game")
+let punchButton = document.getElementById("punchButton")
+let kickButton = document.getElementById("kickButton")
+let specialButton = document.getElementById("specialButton")
+let playerNameEl = document.getElementById("playerName")
+let playerHealthEl = document.getElementById("playerHealth")
+let playerHealthBar = document.getElementById("playerHealthBar")
+let dragonNameEl = document.getElementById("dragonName")
+let dragonHealthEl = document.getElementById("dragonHealth")
+let dragonHealthBar = document.getElementById("dragonHealthBar")
 
 
 let special = 7
@@ -42,23 +42,26 @@ function showGameText(message) {
     game.textContent = message;
 }
 
+
+function disableButtons() {
+    punchButton.disabled = true;
+    kickButton.disabled = true;
+    specialButton.disabled = true;
+}
+
 function playerAttack(attack) {
     showGameText("selectionner votre attaque")
     if (attack === "punch") {
         showGameText(`vous avez donné une punch à ${dragon.name}`)
         dragon.health = dragon.health - 20
-    }
-    else if (attack === "kick") {
+    } else if (attack === "kick") {
         showGameText(`vous avez donné un kick à ${dragon.name}`)
         dragon.health = dragon.health - 25
-    }
-
-    else if (attack === "special") {
+    } else if (attack === "special") {
         showGameText(`vous avez fait votre combo special sur ${dragon.name}`)
         dragon.health = dragon.health - 35
         special = special - 1
-    }
-    else if (attack === "null") {
+    } else if (attack === "null") {
         showGameText("vous recevez des dommages")
         player.health = player.health - 10
     }
@@ -71,69 +74,84 @@ function playerAttack(attack) {
     if (player.health <= 0) {
         showGameText(`Dommage, ${dragon.name} vous a vaincu.`);
         return;
+    }
+    if (player.health <= 0 || dragon.health <= 0) {
+        disableButtons()
+        return;
+    }
+
 }
 
-function specialAttack() {
-    if (special === 0) {
-        showGameText("vous n'avez plus d'attaques spéciales")
+    function specialAttack() {
+        if (special === 0) {
+            showGameText("vous n'avez plus d'attaques spéciales")
+        }
     }
-}
 
-function dragonAttack() {
-    let dragonDammage = 20
-    let dragonSpecialDammage = 30
-    let dragonAttackDammage = Math.random()
-    if (dragonAttackDammage < 0.7) {
-        player.health = player.health - dragonDammage
-        showGameText(`vous recevez une attaque de ${dragon.name}`)
+    function dragonAttack() {
+        let dragonDammage = 20
+        let dragonSpecialDammage = 30
+        let dragonAttackDammage = Math.random()
+        if (dragonAttackDammage < 0.7) {
+            player.health = player.health - dragonDammage
+            showGameText(`vous recevez une attaque de ${dragon.name}`)
+        } else {
+            player.health = player.health - dragonSpecialDammage
+            showGameText(`vous recevez la spéciale de ${dragon.name}`)
+        }
+        showCharacterState()
+
+        if (dragon.health <= 0) {
+            showGameText(`Félicitations ! Vous avez vaincu ${dragon.name}`);
+            return;
+        }
+        if (player.health <= 0) {
+            showGameText(`Dommage, ${dragon.name} vous a vaincu.`);
+            return;
+        }
+        if (player.health <= 0 || dragon.health <= 0) {
+            disableButtons()
+            return;
+        }
     }
-    else {
-        player.health = player.health - dragonSpecialDammage
-        showGameText(`vous recevez la spéciale de ${dragon.name}`)
+
+
+    punchButton.addEventListener("click", function () {
+        if (playerTour) {
+            playerAttack("punch");
+            playerTour = false;
+            setTimeout(playRound, 1000);
+        }
+    });
+
+    kickButton.addEventListener("click", function () {
+        if (playerTour) {
+            playerAttack("kick");
+            playerTour = false;
+            setTimeout(playRound, 1000);
+        }
+    });
+
+    specialButton.addEventListener("click", function () {
+        if (playerTour) {
+            playerAttack("special");
+            playerTour = false;
+            setTimeout(playRound, 1000);
+        }
+    });
+
+    
+    function playRound() {
+        if (!playerTour) {
+            dragonAttack();
+            playerTour = true;
+        }
     }
-    showCharacterState ()
-}
 
 
-punchButton.addEventListener ("click", function () {
-    if (playerTour) {
-        playerAttack("punch");
-        playerTour = false;
-        setTimeout(playRound, 1000);
-    }
-});
-
-kickButton.addEventListener ("click", function () {
-    if (playerTour) {
-        playerAttack("kick");
-        playerTour = false;
-        setTimeout(playRound, 1000);
-    }
-});
-
-specialButton.addEventListener ("click", function () {
-    if (playerTour) {
-        playerAttack("special");
-        playerTour = false;
-        setTimeout(playRound, 1000);
-    }
-});
-
-
-function playRound() {
-    if (!playerTour) {
-        dragonAttack();
-        playerTour = true;
-    }
-}
-
-
-function gameLoop() {
+    function gameLoop() {
         showCharacterState()
         playRound()
     }
 
-    
-gameLoop()
-
-// ecrivez juste bonjour et ajoutez vote en commentaire dans le html, faites attention a ne pas supprimer mon code. meercii chers collègues dev);
+    gameLoop();
